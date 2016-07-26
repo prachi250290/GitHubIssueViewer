bb3ad2a2f43f7d874e31878405749bfe19638c66
Prachi Ghatwal <prachi250290@gmail.com>
2016-07-26 20:51:13 +0530

    Reformatted code

--- GitHubIssueViewer/app/src/main/java/com/test/githubissueviewer/myapplication/ApiInterface.java
+++ GitHubIssueViewer/app/src/main/java/com/test/githubissueviewer/myapplication/ApiInterface.java
@@ -1,13 +1,10 @@
 package com.test.githubissueviewer.myapplication;
 
-import android.graphics.Movie;
-
 import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 
 /**
--- GitHubIssueViewer/app/src/main/java/com/test/githubissueviewer/myapplication/DividerItemDecoration.java
+++ GitHubIssueViewer/app/src/main/java/com/test/githubissueviewer/myapplication/DividerItemDecoration.java
@@ -12,16 +12,13 @@ import android.view.View;
 /**
  * Created by prachi on 26/07/16.
  */
-public class DividerItemDecoration  extends RecyclerView.ItemDecoration {
+public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 
+    public static final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
+    public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
     private static final int[] ATTRS = new int[]{
             android.R.attr.listDivider
     };
-
-    public static final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
-
-    public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
-
     private Drawable mDivider;
 
     private int mOrientation;
